3d2f9486543acf9c3d95618129d9baf8bf5ba278
Jeremi <jeremiur.ps101q@gmail.com>
2023-12-11 18:10:00 -0500

    First valid edit

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,12 @@ mod online;
 //pub use util::*;
 //pub use constants::*;
 use reqwest;
-use std::collections::HashMap;
-use serde::{Deserialize, Serialize};
+//use reqwest::Response;
+use serde::Deserialize;
 use serde_json::Value;
 use std::fs::File;
 use std::io::Read;
-
-use urlencoding::encode;
-
+use std::collections::HashMap;
 
 #[derive(Debug, Deserialize)]
 struct WebData {
@@ -25,116 +23,94 @@ struct WebData {
     acc_tok: String
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-let file_path = "WebData.json";
+fn data() -> WebData {
+    let file_path = "WebData.json";
 
-    // Read the JSON file
     let mut file = File::open(file_path).expect("Unable to open file");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Unable to read file");
 
-    let web_data: WebData = serde_json::from_value(serde_json::from_str(&contents).expect("Err 1")).expect("Err 2");
-
-    // Make an authenticated request using the obtained access token
-    make_authenticated_request(&web_data).await?;
-
-    Ok(())
+    return serde_json::from_value(serde_json::from_str(&contents).expect("Err 1")).expect("Err 2");
 }
 
-async fn make_authenticated_request(web_data: &WebData) -> Result<(), reqwest::Error> {
+fn extract_csrf(json_str: &str) -> Option<String> {
+    let parsed: Result<Value, _> = serde_json::from_str(json_str);
 
-    // Create a client with default settings
+    if let Ok(Value::Object(obj)) = parsed {
+        if let Some(query) = obj.get("query") {
+            if let Some(tokens) = query.get("tokens") {
+                if let Some(csrftoken) = tokens.get("csrftoken") {
+                    if let Some(csrf) = csrftoken.as_str() {
+                        return Some(csrf.to_string());
+                    }
+                }
+            }
+        }
+    }
+    None
+}
+
+async fn csrf_token(web_data: &WebData) -> Result<String, reqwest::Error> {
     let client = reqwest::Client::new();
-    
-    // Set your Wiktionary username and password
-    let username = "";
-    let password = "";
+    let mut params = HashMap::new();
+    params.insert("action", "query");
+    params.insert("meta", "tokens");
+    params.insert("format", "json");
+    params.insert("formatversion", "2");
 
-   
-    // Step 2
-    let mut login_params = HashMap::new();
-    login_params.insert("action", "login");
-    login_params.insert("lgname", username);
-    login_params.insert("lgpassword", password);
-    login_params.insert("lgtoken", &web_data.acc_tok);
-    login_params.insert("format", "json");
-
-    let response = client
-    .post(&web_data.api_url)
-    .form(&login_params)
+    let response = client.post("https://en.wiktionary.org/w/api.php")
+    .header("Authorization", format!("Bearer {}", web_data.acc_tok))
+    .form(&params)
     .send()
     .await?;
 
-    // Parse and print the login response
-    let body = response.text().await?;
-    println!("Login response: {}", body);
+    println!("Status: {}" , response.status());
 
+    let bd = response.text().await?;
+    Ok(extract_csrf(&bd).unwrap())
+}
 
-    let txt = "==Polish==
+async fn edit_wiki_page(web_data: &WebData, csrf_token: &str) -> Result<(), reqwest::Error> {
+    let client = reqwest::Client::new();
+
+    let txt = 
+"==Polish==
 
 ===Pronunciation===
 {{pl-p}}
-
+    
 ===Noun===
 {{head|pl|noun form}}
-
-# {{inflection of|pl|kolorwanka||ins|s}}";
-
-    let mut q_params = HashMap::new();
-        q_params.insert("action", "edit");
-        q_params.insert("action", "edit");
-        q_params.insert("format", "json");
-        q_params.insert("title", "kolorowanką");
-        q_params.insert("text", &txt);
-        q_params.insert("summary", "Added inflection page");
-        q_params.insert("tags","");
-        q_params.insert("bot", "1");
-        q_params.insert("createonly", "1");
-        q_params.insert("contentmodel","wikitext");
-        q_params.insert("token", &web_data.acc_tok);
-        q_params.insert("formatversion", "2");
     
-    let response = client
-        .post(&web_data.api_url)
-        .form(&q_params)
+# {{inflection of|pl|kolorowanka||voc|s}}";
+
+    let mut params = HashMap::new();
+    params.insert("action", "edit");
+    params.insert("title", "kolorowanko");
+    params.insert("appendtext", &txt);
+    params.insert("summary", "Added inflection page");
+    params.insert("tags","");
+    params.insert("bot", "1");
+    params.insert("createonly", "1"); //will not update if exists
+    params.insert("contentmodel","wikitext");
+    params.insert("token", &csrf_token);
+    params.insert("formatversion", "2");
+
+    let response = client.post("https://en.wiktionary.org/w/api.php")
+        .header("Authorization", format!("Bearer {}", web_data.acc_tok))
+        .form(&params)
         .send()
         .await?;
-
-
-    // Process the response as needed
-    println!("Response Status: {:?}", response.status());
-    println!("\n\n\nResponse: {:?}", response.text().await?);
-    println!("Token: {}", &token)
-    ;
-//    println!("\n\n\nFull URL: {}", &encoded_string); 
-
     Ok(())
 }
 
-// Continue with the rest of your code
-//fn main() {
-//    process("okno");
-//    process("koń");
-//    process("kwadrat");
-//    process("nożyk");
-//    process("bezwzględny");
-//    process("pierdolić");
-//    process("chodzić"); //impf-2/3
-  //  process("srać"); //impf 3/3
-//    process("dosrać"); //pf
-//    process("migać");
-//    process("pośredniak");
-//    process("kolorowanka");
-//    process("malowanka");
-//    process("kraszanka");
-//    process("pierdolony");
-//    process("większość_bezwzględna"); -> debug
-//    process("większość_absolutna"); -> debug
-//obiad
-//konitrut
-//wstać
-//pomóc
-//gniewać
-//	online::main();
-//}
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let web_data = data();
+
+    let csrf_token = csrf_token(&web_data).await.expect("NO CSRF");
+
+    edit_wiki_page(&web_data, &csrf_token).await;
+
+    Ok(())
+}
